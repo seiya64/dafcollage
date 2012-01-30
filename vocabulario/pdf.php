5eaa6bcffe13b88f5537b55f9cd6b12623a3e883
simerui <simerui@e382a061-ef60-41ce-baf5-d5b4294055cc>
2012-01-30 12:49:25 +0000

    reparado bug de algunas gramaticas
    git-svn-id: https://forja.rediris.es/svn/cusl6-dafcollag@150 e382a061-ef60-41ce-baf5-d5b4294055cc

--- vocabulario/pdf.php
+++ vocabulario/pdf.php
@@ -315,6 +315,82 @@ if($impr_gram == 1){
                                 $pdf->setLeftMargin(MARGIN);
                                 $pdf->Ln();
                                 break;
+                            //3.1.4 Partizip I
+                            case 24:
+                            //3.3 Trennbare Verben
+                            case 28:
+                            //3.1.5 Futur I
+                            case 25:
+                            //3.1.6 Futur II
+                            case 26:
+                                $pdf->setLeftMargin(MARGIN);
+                                $pdf->SetTextColor(TEXT_AUTO);
+                                $pdf->SetFont('','',10);
+
+                                if($grid==25){
+                                    $titulo=get_string("futuro1", "vocabulario");
+                                }elseif($grid==26){
+                                    $titulo=get_string("futuro2", "vocabulario");
+                                }elseif($grid==28){
+                                    $titulo=get_string('trennbaren','vocabulario');
+                                }elseif($grid==24){
+                                    $titulo=get_string('participio1','vocabulario');
+                                }
+
+                                $pdf->writeHTMLCell(0, 0, 0, 0, '<h3>'.$titulo.'</h3>', 0, 1, 0);
+                                $pdf->setLeftMargin(MARGIN_L2);
+                                $pdf->MultiCell(0, 5, $descripcion_troceada[0], 0, 'J', 0);
+                                $pdf->setLeftMargin(MARGIN);
+                                $pdf->Ln();
+                                break;
+                            //3.1.3 Perfekt/Partizip II
+                            case 23:
+
+                                $pdf->SetTextColor(TEXT_AUTO);
+                                $pdf->SetFont('','',10);
+                                $pdf->SetFillColor(59, 89, 152); //#3B5998
+                                $pdf->setLeftMargin(MARGIN);
+
+                                $pdf->writeHTMLCell(0, 0, 0, 0, '<h3>'.get_string('irregulares','vocabulario').'</h3>', 0, 1, 0);
+                                $pdf->setLeftMargin(MARGIN_L2);
+                                $pdf->MultiCell(0, 5, $descripcion_troceada[0], 0, 'J', 0);
+                                $pdf->setLeftMargin(MARGIN);
+                                $pdf->Ln();
+
+                                $pdf->writeHTMLCell(0, 0, 0, 0, '<h3>'.get_string('participio2','vocabulario').'</h3>', 0, 1, 0);
+                                $pdf->setLeftMargin(MARGIN_L2);
+                                $pdf->MultiCell(0, 5, $descripcion_troceada[1], 0, 'J', 0);
+                                $pdf->setLeftMargin(MARGIN);
+                                $pdf->Ln();
+
+                                $pdf->writeHTMLCell(0, 0, 0, 0, '<h3>'.get_string('hilfsverbs','vocabulario').'</h3>', 0, 1, 0);
+                                $pdf->setLeftMargin(MARGIN_L2);
+                                $pdf->MultiCell(0, 5, $descripcion_troceada[2], 0, 'J', 0);
+                                $pdf->setLeftMargin(MARGIN);
+                                $pdf->Ln();
+
+                                break;
+                            //3.6 Passiv
+                            case 31:
+                                $pdf->SetTextColor(TEXT_AUTO);
+                                $pdf->SetFont('','',10);
+                                $pdf->SetFillColor(59, 89, 152); //#3B5998
+                                $pdf->setLeftMargin(MARGIN);
+
+                                $pdf->writeHTMLCell(0, 0, 0, 0, '<h3>'.get_string('zustandspassiv','vocabulario').'</h3>', 0, 1, 0);
+                                $pdf->setLeftMargin(MARGIN_L2);
+                                $pdf->MultiCell(0, 5, $descripcion_troceada[0], 0, 'J', 0);
+                                $pdf->setLeftMargin(MARGIN);
+                                $pdf->Ln();
+
+                                $pdf->writeHTMLCell(0, 0, 0, 0, '<h3>'.get_string('vorganspassiv','vocabulario').'</h3>', 0, 1, 0);
+                                $pdf->setLeftMargin(MARGIN_L2);
+                                $pdf->MultiCell(0, 5, $descripcion_troceada[1], 0, 'J', 0);
+                                $pdf->setLeftMargin(MARGIN);
+                                $pdf->Ln();
+
+                                break;
+                            
                             //5.2.1
                             case 47:
                                 //Tabla 1
