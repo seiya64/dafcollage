8ccb18333f5509edec71904cc1e7e7aa70d0bd9d
simerui <simerui@e382a061-ef60-41ce-baf5-d5b4294055cc>
2012-03-14 10:55:26 +0000

    git-svn-id: https://forja.rediris.es/svn/cusl6-dafcollag@287 e382a061-ef60-41ce-baf5-d5b4294055cc

---
+++ vocabulario/lang/pl_utf8/vocabulario.php
@@ -0,0 +1,306 @@
+<?php
+
+/*
+Daf-collage is made up of two Moodle modules which help in the process of
+German language learning. It facilitates the content organization like
+vocabulary or the main grammar features and gives the chance to create
+exercises in order to consolidate knowledge.
+
+Copyright (C) 2011
+
+Coordination:
+    Ruth Burbat
+
+Source code:
+    Francisco Javier Rodr√≠guez L√≥pez (seiyadesagitario@gmail.com)
+    Sime√≥n Ruiz Romero (simeonruiz@gmail.com)
+
+Original idea and content design:
+    Ruth Burbat
+    Inmaculada Almahano G√ºeto
+    Andrea Bies
+    Julia M√∂ller Runge
+    Blanca Rodr√≠guez G√≥mez
+    Antonio Salmer√≥n Matilla
+    Mar√≠a Jos√© Varela Salinas
+    Karin Vilar S√°nchez
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.*/
+
+
+
+//TRADUCIDO POR KINGA NOWIK
+
+
+//lo que aqui aparece en aleman es porque tiene que ser asi
+//traducir esas partes al idioma que se quiere ense√±ar para que sirva para cualquiera
+$string['vocabulario'] = 'słownictwo';
+$string['vocabulario_may'] = 'SŁOWNICTWO';
+$string['impr_vocab'] = 'słownictwo';
+$string['impr_gram'] = 'gramatyka';
+$string['impr_tipol'] = 'Rodzajaje tekstów';
+$string['impr_inten'] = 'Intencje komunikacyjne';
+$string['impr_estra'] = 'Strategia nauki';
+$string['add_gram'] = 'Dodać nową gramatykę';
+$string['modulenameplural'] = 'słownictwo';
+$string['modulename'] = 'słownictwo';
+$string['buttonlabel'] = 'zapisz';
+$string['campo_lex'] = 'Grupy tematyczne';
+$string['pal_campo_lex'] = 'Słownictwo według tematów';
+$string['campo_gram'] = 'Gramatyka';
+$string['gramatica_may'] = 'GRAMATYKA';
+$string['tipologias_may'] = 'Rodzaje tekstów';
+$string['estrategias_may'] = 'METODY NAUKI';
+$string['intenciones_may'] = 'INTENCJE KOMUNIKACYJNE';
+$string['campo_intencion'] = 'Intencje komunikacyjne';
+$string['campo_intencion_nuevo'] = 'Nazwa nowej intencji komunikacyjnej';
+$string['campo_gramatica_nueva'] = 'Nazwa nowej aspekt gramatyczny';
+$string['campo_lexico_nuevo'] = 'Nazwa nowej grupy tematyczne';
+$string['campo_tipologia'] = 'Rodzaje tekstów';
+$string['campo_tipologia_nuevo'] = 'Nazwa nowej rodzaj tekstu';
+$string['campo_estrategia_nuevo'] = 'Nazwa nowego zadania';
+$string['campo_estrategia'] = 'Wybrać zadanie';
+$string['miestrategia'] = 'Metody nauki';
+$string['ejem'] = 'Przykład';
+$string['pal'] = 'Słowo';
+$string['Tpal'] = 'Znaczenie';
+$string['gen'] = 'Rodzaj';
+$string['masc'] = 'Męski';
+$string['fem'] = 'Żeński';
+$string['neu'] = 'Nijaki';
+$string['masc_corto'] = 'M';
+$string['fem_corto'] = 'Ż';
+$string['neu_corto'] = 'N';
+$string['plural'] = 'Mnoga';
+$string['sing'] = 'Pojedyncza';
+$string['sust'] = 'Rzeczownik';
+$string['adj'] = 'Przymiotnik';
+$string['vrb'] = 'Czasownik';
+$string['otr'] = 'Inne';
+$string['comen'] = 'Uwagi';
+$string['3per'] = 'Trzecia osoba liczby pojedynczej';
+$string['3perAv'] = '3 os. l. poj.';           //Esto tiene que ser la abreviatura de la anterior
+$string['infi'] = 'Bezokolicznik';
+$string['pret'] = 'Czas przeszły';
+$string['part'] ='Imiesłów';
+$string['pretAv'] = 'Cz. Przeszł.';            //Abreviaci√≥n de la palabra Pret√©rito
+$string['partAv'] = 'Imiesł.';            //Abreviaci√≥n de la palabra Participio
+$string['sindec'] = 'Bez odmiany';
+$string['otr_sus'] = 'Inne';
+$string['desc'] = 'Opis';
+$string['opciones'] = 'Opcje';
+$string['advrb'] = 'Dodać czasownik';
+$string['adadj'] = 'Dodać przymiotnik';
+$string['adotr'] = 'Dodać inne';
+$string['adgr'] = 'Dodać opis';
+$string['error_sus'] = 'Nie wprowadziłeś żadnego słowa do zapisu';
+$string['ver'] = 'Szukać';
+$string['guardar'] = 'Moje słowniciwo';
+$string['editar'] = 'Zmienić';
+$string['eliminar'] = 'Usuń';
+$string['atras'] = 'Cofnij';
+$string['id'] = 'Identyfikator';
+$string['todo'] = 'Słownictwo całość';
+$string['alfabetico'] = 'Słownictwo alfabetycznie';
+$string['nube'] = 'Interaktywny stół';
+$string['pdf'] = 'Kopia pdf';
+$string['revert'] = 'Wyczyść';
+$string['vease_pdf'] = 'Uwagi';
+$string['anotar'] = 'Zanotować';
+$string['busc'] = 'Szukać i uporządkować';
+$string['nuevos'] = 'Utworzyć nowe pole';
+//$string['admin_cl'] = 'Nowy typ tekstu';
+//$string['admin_cl'] = 'Nowe pole tekstowe';
+$string['admin_cl'] = 'Słownictwo';
+$string['admin_gr'] = 'Moja gramatyka';
+$string['admin_ic'] = 'Moje intencje komunikacyjne';
+$string['nueva_ic'] = 'Intencja komunikacyjna';
+$string['nueva_tt'] = 'Rodzaj tekstu';
+$string['nueva_ea'] = 'Metoda nauki';
+$string['admin_tt'] = 'Moje metody nauki';
+$string['admin_ea'] = 'Moje metody uczenia się';
+$string['seccion'] = 'Część';
+$string['nivel'] = 'Wybrać pole: ';
+$string['nivel_estrategia'] = 'Usunąć zadanie';
+$string['alumnos'] = 'Uczniowie kursu';
+$string['pal_gr'] = 'Słownictwo związane z gramatyką';
+$string['pal_ic'] = 'Słownictwo związane z intencjami komunikacyjnymi';
+$string['pal_tt'] = 'Słownictwo związane z tym rodzajem tekstu';
+$string['especificar_gr'] = 'Określić gramatykę';
+$string['especificar_inten'] = 'Określić intencję komunikacyjną';
+$string['especificar_tipo'] = 'Określić typ tekstu';
+$string['referencias'] = 'Przypis';
+$string['ref'] = 'Przyp.';          //Abreviatura de la palabra Referencia
+$string['diccionario'] = 'Słownik';
+$string['emisor'] = 'Nadawca';
+$string['ref_em'] = 'Odniesienie do nadawcy';
+$string['ex_em'] = 'Wykluczenie nadawcy';
+$string['ref_des'] = 'Odniesienie do adresata';
+$string['ex_des'] = 'Wykluczenie adresata';
+$string['elem'] = 'Pokazać część wypowiedzi';
+$string['resu'] = 'Pokazać podsumowanie wypowiedzi';
+$string['verdesc'] = 'Podgląd okna';
+$string['guardesc'] = 'Zapisz zmiany';
+$string['ayuda'] = 'Pomoc';
+$string['cancel'] = 'Menu główne';
+$string['mascampos'] = 'Inne opcje';
+$string['listado'] = 'Pokaż listę';
+//preguntas tipologias textuales
+$string['quien'] = 'Kto przekazuje?';
+$string['finalidad'] = 'Z jaką  intencją?';
+$string['a_quien'] = 'Do kogo?';
+$string['medio'] = 'W jaki sposób?';
+$string['donde'] = 'Gdzie?';
+$string['cuando'] = 'Kiedy?';
+$string['motivo'] = 'W jakim celu?';
+$string['funcion'] = 'Dlaczego?';
+$string['sobre_que'] = 'O czym?';
+$string['que'] = 'Dlaczego';
+$string['orden'] = 'W jakiej kolejności?';
+$string['medios_nonverbales'] = 'Jakimi metodami niewerbalne?';
+$string['que_palabras'] = 'Jakimi słowami?';
+$string['que_frases'] = 'Jakimi zdaniami?';
+$string['que_tono'] = 'Jakim tonem?';
+$string['mastablas'] = 'Dodać nowa tabelę';
+$string['masfilas'] = 'Dodać nową kolumnę';
+$string['muestra_tabla'] = 'Zmienić tabelę';
+$string['nopermisos'] = 'Nie jesteś upoważniony';
+$string['imprcuaderno'] = 'Sciągnij wesję PDF';
+$string['imprimir'] = 'Drukuj';
+$string['cuad_digital_min'] = 'Zeszyt cyfrowy';
+$string['cuad_digital_may'] = 'ZESZYT CYFROWY';
+//descripciones de las intenciones
+$string['desc_inten3.2'] = 'W przeciwieństwie do osiągalnego założenia, założenie hipotetyczne nie odnosi się do faktów lecz do możliwych sytuacji.A diferencia de la condici√≥n realizable la condici√≥n hipot√©tica no se refiere a un hecho real sino a la posible situaci√≥n contraria.';
+$string['desc_inten3.3'] = 'Odnosi się do założenia, który nie może być zrealizowane, gdyż odnosi się do przeszłości.';
+$string['desc_inten4.1'] = 'Odnosi się do zrealizowanych czynów przez istoty żywe/instytucje etc. w celu uzyskania danego efektu.';
+$string['desc_inten4.2'] = 'Odnosi się do zasobów/środków użytych w konkretnym celu.';
+$string['desc_inten5.1'] = 'Odnosi się do oczekiwanego efektu, który nie został osiągnięty.';
+$string['desc_inten5.2'] = 'Odnosi się do oczekiwanego efektu, który ostatecznie nie został osiągnięty.';
+
+///////////////////////////////////////////////////////////
+//a partir de aqui cosas en aleman que NO SE DEBEN TRADUCIR
+///////////////////////////////////////////////////////////
+
+$string['definido'] = 'Definitiv';
+$string['indefinido'] = 'Indefinitiv';
+$string['generales'] = 'Allgemeines';
+$string['particulares'] = 'Lerntipps';
+$string['particular'] = 'Lerntipp';
+$string['declinacion1'] = 'Deklination Typ 1';
+$string['declinacion2'] = 'Deklination Typ 2';
+$string['declinacion3'] = 'Deklination Typ 3';
+$string['declinacion4'] = 'Deklinationsendungen';
+$string['declinacion_siehe'] = 'Deklinationsendungen (siehe auch 2.4 Indefinitpronomen)';
+$string['masculino'] = 'Maskulin';
+$string['neutro'] = 'Neutrum';
+$string['femenino'] = 'Feminin';
+$string['pluralAl'] = 'Plural';
+$string['singAl'] = 'Singular';
+$string['nominativo'] = 'Nominativ';
+$string['acusativo'] = 'Akkusativ';
+$string['dativo'] = 'Dativ';
+$string['acudat'] = 'Akkusativ + Dativ';
+$string['genitivo'] = 'Genitiv';
+$string['indicativo'] = 'Indikativ';
+$string['conjuntivo'] = 'Konjunktiv';
+$string['conjuntivo1'] = 'Konjunktiv I';
+$string['conjuntivo2'] = 'Konjunktiv II';
+$string['preterito'] = 'Präteritum';
+$string['prasens'] = 'Präsens';
+$string['perfecto'] = 'Perfekt';
+$string['clasificacionsemantica'] = 'Klassifizierung nach semantischen Aspekten';
+$string['clasificacionformal'] = 'Klassifizierung nach formalen Aspekten';
+$string['endungs'] = 'Endung Singular';
+$string['endungp'] = 'Endung Plural';
+$string['genero'] = 'Genus';
+$string['reinesf'] = 'Reine Singularformen';
+$string['reinepf'] = 'Reine Pluralformen';
+$string['beispielsatz'] = 'Beispielsatz';
+$string['beispiele_def'] = 'Beispiele Definitartikel';
+$string['beispiele_indef'] = 'Beispiele Indefinitartikel';
+$string['beispiele_null'] = 'Beispiele Nullartikel';
+$string['satzart'] = 'Satzart';
+$string['komfun'] = 'Kommunikative Funktion';
+$string['person'] = 'Person';
+$string['nichtperson'] = 'Nicht-Person';
+$string['satztr'] = 'Satzstruktur';
+$string['subjunktor'] = 'Subjunktor';
+$string['subjekt'] = 'Subjekt';
+//abreviaturas con una s (de small) delante
+$string['snominativo'] = 'N';
+$string['sacusativo'] = 'A';
+$string['sdativo'] = 'D';
+$string['sgenitivo'] = 'G';
+//personas
+$string['S1'] = 'ich';
+$string['S2'] = 'du';
+$string['S3'] = 'er/sie/es';
+$string['P1'] = 'wir';
+$string['P2'] = 'ihr';
+$string['P3'] = 'sie/Sie';
+$string['sie'] = 'Sie';
+//seguimos con mas cosas
+$string['beachten'] = 'Beachten';
+$string['atencion_may'] = 'BEACHTEN!!';
+$string['participio1'] = 'Regeln zur Bildung des Partizip I';
+$string['participio2'] = 'Regeln zur Bildung des Partizip II';
+$string['futuro1'] = 'Regeln zur Bildung des Futur I';
+$string['futuro2'] = 'Regeln zur Bildung des Futur II';
+$string['hilfsverbs'] = 'Regeln zum Gebrauch des Hilfsverbs';
+$string['trennbaren'] = 'Regeln zur Bildung der trennbaren Verben';
+$string['zustandspassiv'] = 'Zustandspassiv (sein-Passiv)';
+$string['vorganspassiv'] = 'Vorganspassiv (werden-Passiv)';
+$string['sein'] = 'Sein';
+$string['andere'] = 'Andere Verben';
+$string['lista'] = 'Liste der Artikelwörter';
+$string['scheinbare'] = 'Scheinbare Artikelwörter';
+$string['temporal'] = 'Temporal';
+$string['causal'] = 'Kausal';
+$string['modal'] = 'Modal';
+$string['local'] = 'Lokal';
+$string['func'] = 'Funktion';
+$string['siehe'] = 'Siehe auch';
+$string['satzstruktur'] = 'SATZSTRUKTUR';
+$string['vorfeld'] = 'Vorfeld';
+$string['konjugier'] = 'Konjugiertes Verb';
+$string['mittelfeld'] = 'Mittelfeld';
+$string['mittel'] = 'Mittel';
+$string['wortklase'] = 'Theoretische Beschreibung';
+$string['verb2'] = 'Verb 2';
+$string['verb1'] = 'Verb 1';
+$string['miraren'] = 'Siehe auch';
+$string['schwache'] = 'Schwache Verben';
+$string['schwache_siehe'] = 'Schwache Verben (siehe auch 3.2.2 Präteritum)';
+$string['starke'] = 'Starke Verben';
+$string['gemischte'] = 'Gemischte Verben';
+$string['possessiv1'] = 'Possessivpronomina ohne Deklination';
+$string['possessiv2'] = 'Possessivartikel ohne Deklination';
+$string['irregulares'] = 'Unregelmäßige Verben';
+$string['gebrauch'] = 'Gebrauch';
+$string['reflexivo'] = 'Reflexivpronomen';
+$string['positivo'] = 'Positiv';
+$string['infinitivo'] = 'Infinitiv';
+$string['comparativo'] = 'Komparativ';
+$string['superlativo'] = 'Superlativ';
+$string['despuesde'] = 'Steht nach';
+$string['endungen_siehe1'] = 'Endungen siehe auch 4.2 Indefinitartikel auβer Pluralformen';
+$string['endungen_siehe2'] = 'Endungen siehe auch 4.1 Definitartikel + vorangestelltem e';
+$string['endungen_siehe3'] = 'Endungen siehe auch 4.1 Definitartikel + teilweise vorangestelltem e, sowie 4.5 Interrogativartikel';
+$string['endungen_siehe4'] = 'Endungen siehe auch 4.2 Indefinitartikel auβer Pluralformen, sowie 4.4 Negationsartikel';
+$string['praposit'] = 'Präposition';
+$string['beisp'] = 'Beispiel';
+$string['par_schwac'] = '(= Regelmäβige Verben)';
+$string['par_star'] = '(= Verben mit bestimmten gemeinsamen Unregelmäβigkeiten)';
+$string['par_gemis'] = '(= Unregelmäβige Verben)';
+$string['kas'] = 'Kasus';
+$string['preposiciones']='Präpositionen';
+
+?>
