cf07dd28a703aeca74cfcceb3229f9953dc96ddb
simerui <simerui@e382a061-ef60-41ce-baf5-d5b4294055cc>
2012-03-16 10:05:06 +0000

    formateo del código
    git-svn-id: https://forja.rediris.es/svn/cusl6-dafcollag@314 e382a061-ef60-41ce-baf5-d5b4294055cc

--- vocabulario/lang/de_utf8/vocabulario.php
+++ vocabulario/lang/de_utf8/vocabulario.php
@@ -1,41 +1,41 @@
 <?php
 
 /*
-Daf-collage is made up of two Moodle modules which help in the process of
-German language learning. It facilitates the content organization like
-vocabulary or the main grammar features and gives the chance to create
-exercises in order to consolidate knowledge.
+  Daf-collage is made up of two Moodle modules which help in the process of
+  German language learning. It facilitates the content organization like
+  vocabulary or the main grammar features and gives the chance to create
+  exercises in order to consolidate knowledge.
 
-Copyright (C) 2011
+  Copyright (C) 2011
 
-Coordination:
-    Ruth Burbat
+  Coordination:
+  Ruth Burbat
 
-Source code:
-    Francisco Javier Rodríguez López (seiyadesagitario@gmail.com)
-    Simeón Ruiz Romero (simeonruiz@gmail.com)
+  Source code:
+  Francisco Javier Rodríguez López (seiyadesagitario@gmail.com)
+  Simeón Ruiz Romero (simeonruiz@gmail.com)
 
-Original idea and content design:
-    Ruth Burbat
-    Inmaculada Almahano Güeto
-    Andrea Bies
-    Julia Möller Runge
-    Blanca Rodríguez Gómez
-    Antonio Salmerón Matilla
-    María José Varela Salinas
-    Karin Vilar Sánchez
-	
-	Traducido por Alicia Casado Valenzuela
+  Original idea and content design:
+  Ruth Burbat
+  Inmaculada Almahano Güeto
+  Andrea Bies
+  Julia Möller Runge
+  Blanca Rodríguez Gómez
+  Antonio Salmerón Matilla
+  María José Varela Salinas
+  Karin Vilar Sánchez
 
-This program is free software; you can redistribute it and/or
-modify it under the terms of the GNU General Public License
-as published by the Free Software Foundation; either version 2
-of the License, or (at your option) any later version.
+  Traducido por Alicia Casado Valenzuela
 
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.*/
+  This program is free software; you can redistribute it and/or
+  modify it under the terms of the GNU General Public License
+  as published by the Free Software Foundation; either version 2
+  of the License, or (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details. */
 
 
 //lo que aqui aparece en aleman es porque tiene que ser asi
@@ -297,6 +297,5 @@ $string['par_schwac'] = '(= Regelmäβige Verben)';
 $string['par_star'] = '(= Verben mit bestimmten gemeinsamen Unregelmäβigkeiten)';
 $string['par_gemis'] = '(= Unregelmäβige Verben)';
 $string['kas'] = 'Kasus';
-$string['preposiciones']='Präpositionen';
-
+$string['preposiciones'] = 'Präpositionen';
 ?>
