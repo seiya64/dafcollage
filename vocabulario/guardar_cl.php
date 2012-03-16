b1464a5e1e094d99fc686d2572ac4257456b23ab
simerui <simerui@e382a061-ef60-41ce-baf5-d5b4294055cc>
2012-03-16 10:03:39 +0000

    formateo del código
    git-svn-id: https://forja.rediris.es/svn/cusl6-dafcollag@312 e382a061-ef60-41ce-baf5-d5b4294055cc

--- vocabulario/guardar_cl.php
+++ vocabulario/guardar_cl.php
@@ -1,39 +1,39 @@
 <?php
 
 /*
-Daf-collage is made up of two Moodle modules which help in the process of
-German language learning. It facilitates the content organization like
-vocabulary or the main grammar features and gives the chance to create
-exercises in order to consolidate knowledge.
+  Daf-collage is made up of two Moodle modules which help in the process of
+  German language learning. It facilitates the content organization like
+  vocabulary or the main grammar features and gives the chance to create
+  exercises in order to consolidate knowledge.
 
-Copyright (C) 2011
+  Copyright (C) 2011
 
-Coordination:
-    Ruth Burbat
+  Coordination:
+  Ruth Burbat
 
-Source code:
-    Francisco Javier Rodríguez López (seiyadesagitario@gmail.com)
-    Simeón Ruiz Romero (simeonruiz@gmail.com)
+  Source code:
+  Francisco Javier Rodríguez López (seiyadesagitario@gmail.com)
+  Simeón Ruiz Romero (simeonruiz@gmail.com)
 
-Original idea and content design:
-    Ruth Burbat
-    Inmaculada Almahano Güeto
-    Andrea Bies
-    Julia Möller Runge
-    Blanca Rodríguez Gómez
-    Antonio Salmerón Matilla
-    María José Varela Salinas
-    Karin Vilar Sánchez
+  Original idea and content design:
+  Ruth Burbat
+  Inmaculada Almahano Güeto
+  Andrea Bies
+  Julia Möller Runge
+  Blanca Rodríguez Gómez
+  Antonio Salmerón Matilla
+  María José Varela Salinas
+  Karin Vilar Sánchez
 
-This program is free software; you can redistribute it and/or
-modify it under the terms of the GNU General Public License
-as published by the Free Software Foundation; either version 2
-of the License, or (at your option) any later version.
+  This program is free software; you can redistribute it and/or
+  modify it under the terms of the GNU General Public License
+  as published by the Free Software Foundation; either version 2
+  of the License, or (at your option) any later version.
 
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.*/
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details. */
 
 
 require_once("../../config.php");
@@ -62,8 +62,8 @@ if (optional_param('eliminar', 0, PARAM_INT) && $campo['padre'] > 106) {
     //comenzamos una transacción para que en todas las tablas se haga seguido
     // en caso de error en algun delete, no se hace ninguno
     begin_sql();
-    foreach ($sufijos as $sufijo){
-        delete_records('vocabulario_camposlexicos_'.$sufijo, 'id', $campo['padre']);
+    foreach ($sufijos as $sufijo) {
+        delete_records('vocabulario_camposlexicos_' . $sufijo, 'id', $campo['padre']);
     }
     //confirmamos la transacción
     commit_sql();
@@ -74,8 +74,8 @@ if ($campo['campo'] != null) {
     //comenzamos una transacción para que todos los insert sean seguidos y produzcan el mismo id en todas las tablas
     //en caso de error en uno, no se hace ninguno
     begin_sql();
-    foreach($sufijos as $sufijo){
-        insert_record('vocabulario_camposlexicos_'.$sufijo, $campo, true);
+    foreach ($sufijos as $sufijo) {
+        insert_record('vocabulario_camposlexicos_' . $sufijo, $campo, true);
     }
     //confirmamos la transacción
     commit_sql();
