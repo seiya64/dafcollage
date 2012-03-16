2b1c022ef6312e77483f100e546d90e641bdb503
simerui <simerui@e382a061-ef60-41ce-baf5-d5b4294055cc>
2012-03-16 08:45:14 +0000

    formateo del código
    git-svn-id: https://forja.rediris.es/svn/cusl6-dafcollag@294 e382a061-ef60-41ce-baf5-d5b4294055cc

--- vocabulario/funciones.js
+++ vocabulario/funciones.js
@@ -41,37 +41,37 @@ GNU General Public License for more details.
  */
 
 function traducir(elemento) {
-	var palabra;
-	switch (elemento) {
-		case 1:
-			palabra = document.getElementById("id_palabra_sus").value;
-			break;
-		case 2:
-			palabra = document.getElementById("id_infinitivo").value;
-			break;
-		case 3:
-			palabra = document.getElementById("id_sindeclinar").value;
-			break;
-		case 4:
-			palabra = document.getElementById("id_palabra_otr").value;
-			break;
-		default:
-			break;
-	}
-	var url;
-	var dic = document.getElementById("id_diccionario").value;
-	if(dic == 1) {
-		url = "http://dict.leo.org/esde?lp=esde&lang=de&search=" + palabra;
-	} else if(dic == 0) {
-		url = "http://www.dwds.de/?qu=" + palabra + "&view=1";
-	} else if(dic == 2) {
-		url = "http://de.pons.eu/dict/search/results/?q=" + palabra + "&l=dees";
-	} else if(dic == 3) {
-		url = "http://www.openthesaurus.de/synonyme/" + palabra;
-	} else if(dic == 4) {
-		url = "http://www.duden.de/suchen/dudenonline/" + palabra;
-	}
-	window.open(url, 'diccionario');
+    var palabra;
+    switch (elemento) {
+        case 1:
+            palabra = document.getElementById("id_palabra_sus").value;
+            break;
+        case 2:
+            palabra = document.getElementById("id_infinitivo").value;
+            break;
+        case 3:
+            palabra = document.getElementById("id_sindeclinar").value;
+            break;
+        case 4:
+            palabra = document.getElementById("id_palabra_otr").value;
+            break;
+        default:
+            break;
+    }
+    var url;
+    var dic = document.getElementById("id_diccionario").value;
+    if(dic == 1) {
+        url = "http://dict.leo.org/esde?lp=esde&lang=de&search=" + palabra;
+    } else if(dic == 0) {
+        url = "http://www.dwds.de/?qu=" + palabra + "&view=1";
+    } else if(dic == 2) {
+        url = "http://de.pons.eu/dict/search/results/?q=" + palabra + "&l=dees";
+    } else if(dic == 3) {
+        url = "http://www.openthesaurus.de/synonyme/" + palabra;
+    } else if(dic == 4) {
+        url = "http://www.duden.de/suchen/dudenonline/" + palabra;
+    }
+    window.open(url, 'diccionario');
 }
 
 /**
@@ -83,32 +83,32 @@ function traducir(elemento) {
 
 function cargaContenido(miselect, otroselect, tipo) {
 
-	var elselect = document.getElementById(miselect).value;
-	var nombre = document.getElementById(miselect).name;
+    var elselect = document.getElementById(miselect).value;
+    var nombre = document.getElementById(miselect).name;
 
-	if(elselect == 0 || elselect == 1) {
-		document.getElementById(otroselect).innerHTML = "";
-	} else {
-		document.getElementById(otroselect).innerHTML = '<p style="text-align: center;"><img src="./imagenes/loading.gif"/></p>';
-		if(window.XMLHttpRequest) {//para que se vea en IE7+, Firefox, Chrome, Opera, Safari
-			xmlhttp = new XMLHttpRequest();
-		} else {//para que se vea en IE6, IE5 y otros
-			xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
-		}
-		xmlhttp.onreadystatechange = function() {
-			if(xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-				document.getElementById(otroselect).innerHTML = xmlhttp.responseText;
-			}
-		}
-		if(tipo == 0) {//campos lexicos
-			xmlhttp.open("GET", "subcampos.php?id=" + elselect + "&nombre=" + nombre, true);
-		} else if(tipo == 1) {//gramaticas
-			xmlhttp.open("GET", "subgramaticas.php?id=" + elselect + "&nombre=" + nombre, true);
-		} else if(tipo == 2) {//intenciones comunicativas
-			xmlhttp.open("GET", "subintenciones.php?id=" + elselect + "&nombre=" + nombre, true);
-		}
-		xmlhttp.send();
-	}
+    if(elselect == 0 || elselect == 1) {
+        document.getElementById(otroselect).innerHTML = "";
+    } else {
+        document.getElementById(otroselect).innerHTML = '<p style="text-align: center;"><img src="./imagenes/loading.gif"/></p>';
+        if(window.XMLHttpRequest) {//para que se vea en IE7+, Firefox, Chrome, Opera, Safari
+            xmlhttp = new XMLHttpRequest();
+        } else {//para que se vea en IE6, IE5 y otros
+            xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+        }
+        xmlhttp.onreadystatechange = function() {
+            if(xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+                document.getElementById(otroselect).innerHTML = xmlhttp.responseText;
+            }
+        }
+        if(tipo == 0) {//campos lexicos
+            xmlhttp.open("GET", "subcampos.php?id=" + elselect + "&nombre=" + nombre, true);
+        } else if(tipo == 1) {//gramaticas
+            xmlhttp.open("GET", "subgramaticas.php?id=" + elselect + "&nombre=" + nombre, true);
+        } else if(tipo == 2) {//intenciones comunicativas
+            xmlhttp.open("GET", "subintenciones.php?id=" + elselect + "&nombre=" + nombre, true);
+        }
+        xmlhttp.send();
+    }
 }
 
 /**
@@ -119,10 +119,10 @@ function cargaContenido(miselect, otroselect, tipo) {
  */
 
 function ocultar(seccion) {
-	ocu = document.getElementById("ocultador_" + seccion);
-	ocu.style.display = "none";
-	enlace = document.getElementById("mc" + seccion);
-	enlace.href = "javascript:desocultar('" + seccion + "')";
+    ocu = document.getElementById("ocultador_" + seccion);
+    ocu.style.display = "none";
+    enlace = document.getElementById("mc" + seccion);
+    enlace.href = "javascript:desocultar('" + seccion + "')";
 }
 
 /**
@@ -134,10 +134,10 @@ function ocultar(seccion) {
  */
 
 function desocultar(seccion) {
-	ocu = document.getElementById("ocultador_" + seccion);
-	ocu.style.display = "";
-	enlace = document.getElementById("mc" + seccion);
-	enlace.href = "javascript:ocultar('" + seccion + "')";
+    ocu = document.getElementById("ocultador_" + seccion);
+    ocu.style.display = "";
+    enlace = document.getElementById("mc" + seccion);
+    enlace.href = "javascript:ocultar('" + seccion + "')";
 }
 
 /**
@@ -148,8 +148,8 @@ function desocultar(seccion) {
  */
 
 function desactivar_todo(ini) {
-	for( i = ini; i < document.getElementById("mform1").elements.length - 3; i++)
-	document.getElementById("mform1").elements[i].disabled = true;
+    for( i = ini; i < document.getElementById("mform1").elements.length - 3; i++)
+        document.getElementById("mform1").elements[i].disabled = true;
 }
 
 /**
@@ -158,8 +158,8 @@ function desactivar_todo(ini) {
  */
 
 function activar_todo() {
-	for( i = 0; i < document.getElementById("mform1").elements.length; i++)
-	document.getElementById("mform1").elements[i].disabled = false;
+    for( i = 0; i < document.getElementById("mform1").elements.length; i++)
+        document.getElementById("mform1").elements[i].disabled = false;
 }
 
 /**
@@ -167,10 +167,10 @@ function activar_todo() {
  */
 
 function comprobar_cookies(){
-if (navigator.cookieEnabled == 0) {
+    if (navigator.cookieEnabled == 0) {
         alert("Este sitio necesita tener habilitadas cookies. Pulse aceptar para saber cómo puede habilitarlas");
-		window.open('./habilitar_cookies.html','','location=no,directories=no, status=no,menubar=no,resizable=no');
-	}
+        window.open('./habilitar_cookies.html','','location=no,directories=no, status=no,menubar=no,resizable=no');
+    }
 }
 
 /**
@@ -178,23 +178,23 @@ if (navigator.cookieEnabled == 0) {
  */
 
 function la_tabla_nube() {
-               var oTable = $('#palabras').dataTable({
-                    "bJQueryUI": true,
-                    "oLanguage": {
-                        "sLengthMenu": "Mostrar _MENU_ entradas por página",
-                        "sZeroRecords": "No se encontró nada",
-                        "sInfo": "Mostrando _START_ de _END_ de un total de _TOTAL_ entradas",
-                        "sInfoEmpty": "Mostrando 0 de 0 de un total de 0 entradas",
-                        "sInfoFiltered": "(Filtrado de _MAX_ entradas)"
-                    }
+    var oTable = $('#palabras').dataTable({
+        "bJQueryUI": true,
+        "oLanguage": {
+            "sLengthMenu": "Mostrar _MENU_ entradas por página",
+            "sZeroRecords": "No se encontró nada",
+            "sInfo": "Mostrando _START_ de _END_ de un total de _TOTAL_ entradas",
+            "sInfoEmpty": "Mostrando 0 de 0 de un total de 0 entradas",
+            "sInfoFiltered": "(Filtrado de _MAX_ entradas)"
+        }
 
 
-                });
+    });
 
 
-            $('#palabras tbody tr td').live('click', function (){
-                var nTds = $(this);
-                //alert (nTds.html());
-                oTable.fnFilter(nTds.html());
-               });
+    $('#palabras tbody tr td').live('click', function (){
+        var nTds = $(this);
+        //alert (nTds.html());
+        oTable.fnFilter(nTds.html());
+    });
 }
