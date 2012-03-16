704a948a60a61f479575860ed8495d66817af267
simerui <simerui@e382a061-ef60-41ce-baf5-d5b4294055cc>
2012-03-16 10:01:56 +0000

    formateo del código
    git-svn-id: https://forja.rediris.es/svn/cusl6-dafcollag@306 e382a061-ef60-41ce-baf5-d5b4294055cc

--- vocabulario/guardar_tt.php
+++ vocabulario/guardar_tt.php
@@ -1,39 +1,39 @@
 <?php
 
 /*
-Daf-collage is made up of two Moodle modules which help in the process of
-German language learning. It facilitates the content organization like
-vocabulary or the main grammar features and gives the chance to create
-exercises in order to consolidate knowledge.
+  Daf-collage is made up of two Moodle modules which help in the process of
+  German language learning. It facilitates the content organization like
+  vocabulary or the main grammar features and gives the chance to create
+  exercises in order to consolidate knowledge.
 
-Copyright (C) 2011
+  Copyright (C) 2011
 
-Coordination:
-    Ruth Burbat
+  Coordination:
+  Ruth Burbat
 
-Source code:
-    Francisco Javier Rodríguez López (seiyadesagitario@gmail.com)
-    Simeón Ruiz Romero (simeonruiz@gmail.com)
+  Source code:
+  Francisco Javier Rodríguez López (seiyadesagitario@gmail.com)
+  Simeón Ruiz Romero (simeonruiz@gmail.com)
 
-Original idea and content design:
-    Ruth Burbat
-    Inmaculada Almahano Güeto
-    Andrea Bies
-    Julia Möller Runge
-    Blanca Rodríguez Gómez
-    Antonio Salmerón Matilla
-    María José Varela Salinas
-    Karin Vilar Sánchez
+  Original idea and content design:
+  Ruth Burbat
+  Inmaculada Almahano Güeto
+  Andrea Bies
+  Julia Möller Runge
+  Blanca Rodríguez Gómez
+  Antonio Salmerón Matilla
+  María José Varela Salinas
+  Karin Vilar Sánchez
 
-This program is free software; you can redistribute it and/or
-modify it under the terms of the GNU General Public License
-as published by the Free Software Foundation; either version 2
-of the License, or (at your option) any later version.
+  This program is free software; you can redistribute it and/or
+  modify it under the terms of the GNU General Public License
+  as published by the Free Software Foundation; either version 2
+  of the License, or (at your option) any later version.
 
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.*/
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details. */
 
 require_once("../../config.php");
 require_once("lib.php");
@@ -55,25 +55,25 @@ $tipologia = new Vocabulario_tipologias($user_object->id, required_param('campot
 
 //print_object($tipologia);
 $desc = '';
-for ($i = 1; $i < 6; $i++){
-    $desc .= optional_param('quien'.$i, null, PARAM_TEXT) . __SEPARADORCAMPOS__ . optional_param('finalidad'.$i, null, PARAM_TEXT) . __SEPARADORCAMPOS__;
-    $desc .= optional_param('a_quien'.$i, null, PARAM_TEXT) . __SEPARADORCAMPOS__ . optional_param('medio'.$i, null, PARAM_TEXT). __SEPARADORCAMPOS__;
-    $desc .= optional_param('donde'.$i, null, PARAM_TEXT) . __SEPARADORCAMPOS__ . optional_param('cuando'.$i, null, PARAM_TEXT). __SEPARADORCAMPOS__;
-    $desc .= optional_param('motivo'.$i, null, PARAM_TEXT) . __SEPARADORCAMPOS__ . optional_param('funcion'.$i, null, PARAM_TEXT). __SEPARADORCAMPOS__;
-    $desc .= optional_param('sobre_que'.$i, null, PARAM_TEXT) . __SEPARADORCAMPOS__ . optional_param('que'.$i, null, PARAM_TEXT). __SEPARADORCAMPOS__;
-    $desc .= optional_param('orden'.$i, null, PARAM_TEXT) . __SEPARADORCAMPOS__ . optional_param('medios_nonverbales'.$i, null, PARAM_TEXT). __SEPARADORCAMPOS__;
-    $desc .= optional_param('que_palabras'.$i, null, PARAM_TEXT) . __SEPARADORCAMPOS__ . optional_param('que_frases'.$i, null, PARAM_TEXT). __SEPARADORCAMPOS__;
-    $desc .= optional_param('que_tono'.$i, null, PARAM_TEXT).__SEPARADORCAMPOS__;
+for ($i = 1; $i < 6; $i++) {
+    $desc .= optional_param('quien' . $i, null, PARAM_TEXT) . __SEPARADORCAMPOS__ . optional_param('finalidad' . $i, null, PARAM_TEXT) . __SEPARADORCAMPOS__;
+    $desc .= optional_param('a_quien' . $i, null, PARAM_TEXT) . __SEPARADORCAMPOS__ . optional_param('medio' . $i, null, PARAM_TEXT) . __SEPARADORCAMPOS__;
+    $desc .= optional_param('donde' . $i, null, PARAM_TEXT) . __SEPARADORCAMPOS__ . optional_param('cuando' . $i, null, PARAM_TEXT) . __SEPARADORCAMPOS__;
+    $desc .= optional_param('motivo' . $i, null, PARAM_TEXT) . __SEPARADORCAMPOS__ . optional_param('funcion' . $i, null, PARAM_TEXT) . __SEPARADORCAMPOS__;
+    $desc .= optional_param('sobre_que' . $i, null, PARAM_TEXT) . __SEPARADORCAMPOS__ . optional_param('que' . $i, null, PARAM_TEXT) . __SEPARADORCAMPOS__;
+    $desc .= optional_param('orden' . $i, null, PARAM_TEXT) . __SEPARADORCAMPOS__ . optional_param('medios_nonverbales' . $i, null, PARAM_TEXT) . __SEPARADORCAMPOS__;
+    $desc .= optional_param('que_palabras' . $i, null, PARAM_TEXT) . __SEPARADORCAMPOS__ . optional_param('que_frases' . $i, null, PARAM_TEXT) . __SEPARADORCAMPOS__;
+    $desc .= optional_param('que_tono' . $i, null, PARAM_TEXT) . __SEPARADORCAMPOS__;
 }
 
-$desc .=  optional_param('miraren', null, PARAM_TEXT);
+$desc .= optional_param('miraren', null, PARAM_TEXT);
 
 //vemos que botón hemos pulsado
-if ($mform->no_submit_button_pressed()){
-    if(optional_param('desc_btn')){
+if ($mform->no_submit_button_pressed()) {
+    if (optional_param('desc_btn')) {
         $tipologiaaux = new Vocabulario_mis_tipologias();
         $tipologiaaux->leer($tipologia->get('padre'));
-        $tipologiaaux->set($USER->id,$tipologia->get('padre'),$desc);
+        $tipologiaaux->set($USER->id, $tipologia->get('padre'), $desc);
 //        print_object($tipologiaaux);
         $tipologiaaux->guardar();
         //$ttidaux = update_record('vocabulario_mis_tipologias', $tipologiaaux, true);
