179459df381d8190cb31bbb76b98e91e95d90ee3
frantorres <frantorres@e382a061-ef60-41ce-baf5-d5b4294055cc>
2012-04-15 18:30:14 +0000

    Enlace a menú principal
    git-svn-id: https://forja.rediris.es/svn/cusl6-dafcollag@372 e382a061-ef60-41ce-baf5-d5b4294055cc

--- vocabulario/vocabulario_formularios.php
+++ vocabulario/vocabulario_formularios.php
@@ -106,8 +106,7 @@ class mod_vocabulario_rellenar_form extends moodleform {
         //inclusion del css para que se vean en dos columnas
         //$mform->addElement('html', '<script type="text/javascript" src="funciones.js"></script>');
         //titulo de la seccion
-        $mform->addElement('html', '<h1>' . get_string('guardar', 'vocabulario') . '</h1>');
-
+        $mform->addElement('html', '<h1>' . get_string('guardar', 'vocabulario') . '<a href="view.php" onclick="skipClientValidation = true; return true;" id="id_cancellink">Menú principal</a></h1>');
         if ($leido) {
             $mform->addElement('hidden', 'idleido', $leido);
         }
