836422a466dca46372f2e07e40e35f16e00f3050
finamolina <finamolina@e382a061-ef60-41ce-baf5-d5b4294055cc>
2012-05-07 10:31:36 +0000

    Añadidos ficheros iniciales del nuevo modulo
    git-svn-id: https://forja.rediris.es/svn/cusl6-dafcollag@408 e382a061-ef60-41ce-baf5-d5b4294055cc

---
+++ ejercicios/lang/en_utf8/newmodule.php
@@ -0,0 +1,12 @@
+<?php
+
+$string['newmodule'] = 'newmodule';
+
+$string['modulename'] = 'newmodule';
+$string['modulenameplural'] = 'newmoduleS';
+
+$string['newmodulefieldset'] = 'Custom example fieldset';
+$string['newmoduleintro'] = 'newmodule Intro';
+$string['newmodulename'] = 'newmodule Name';
+
+?>
