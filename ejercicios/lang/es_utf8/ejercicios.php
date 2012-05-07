014b53997feaa225f34df86cf66cd5fa0cb52895
finamolina <finamolina@e382a061-ef60-41ce-baf5-d5b4294055cc>
2012-05-07 10:45:31 +0000

    Añadidos ficheros iniciales del nuevo modulo
    git-svn-id: https://forja.rediris.es/svn/cusl6-dafcollag@411 e382a061-ef60-41ce-baf5-d5b4294055cc

---
+++ ejercicios/lang/es_utf8/ejercicios.php
@@ -0,0 +1,12 @@
+<?php
+
+$string['ejercicios'] = 'ejercicios';
+
+$string['modulename'] = 'ejercicios';
+$string['modulenameplural'] = 'ejercicios';
+
+$string['ejerciciosfieldset'] = 'Custom example fieldset';
+$string['ejerciciosintro'] = 'ejercicios Intro';
+$string['ejerciciosname'] = 'ejercicios Name';
+
+?>
