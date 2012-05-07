453b7c1f34eeb08e9df28d7dc9f69b732c5cd0ff
finamolina <finamolina@e382a061-ef60-41ce-baf5-d5b4294055cc>
2012-05-07 10:28:45 +0000

    Añadidos ficheros iniciales del nuevo modulo
    git-svn-id: https://forja.rediris.es/svn/cusl6-dafcollag@403 e382a061-ef60-41ce-baf5-d5b4294055cc

--- ejercicios/view.php
+++ ejercicios/view.php
@@ -1,23 +1,23 @@
 <?php  // $Id: view.php,v 1.6.2.3 2009/04/17 22:06:25 skodak Exp $
 
 /**
- * This page prints a particular instance of ejercicio
+ * This page prints a particular instance of newmodule
  *
  * @author  Your Name <your@email.address>
  * @version $Id: view.php,v 1.6.2.3 2009/04/17 22:06:25 skodak Exp $
- * @package mod/ejercicio
+ * @package mod/newmodule
  */
 
-/// (Replace ejercicio with the name of your module and remove this line)
+/// (Replace newmodule with the name of your module and remove this line)
 
 require_once(dirname(dirname(dirname(__FILE__))).'/config.php');
 require_once(dirname(__FILE__).'/lib.php');
 
 $id = optional_param('id', 0, PARAM_INT); // course_module ID, or
-$a  = optional_param('a', 0, PARAM_INT);  // ejercicio instance ID
+$a  = optional_param('a', 0, PARAM_INT);  // newmodule instance ID
 
 if ($id) {
-    if (! $cm = get_coursemodule_from_id('ejercicio', $id)) {
+    if (! $cm = get_coursemodule_from_id('newmodule', $id)) {
         error('Course Module ID was incorrect');
     }
 
@@ -25,18 +25,18 @@ if ($id) {
         error('Course is misconfigured');
     }
 
-    if (! $ejercicio = get_record('ejercicio', 'id', $cm->instance)) {
+    if (! $newmodule = get_record('newmodule', 'id', $cm->instance)) {
         error('Course module is incorrect');
     }
 
 } else if ($a) {
-    if (! $ejercicio = get_record('ejercicio', 'id', $a)) {
+    if (! $newmodule = get_record('newmodule', 'id', $a)) {
         error('Course module is incorrect');
     }
-    if (! $course = get_record('course', 'id', $ejercicio->course)) {
+    if (! $course = get_record('course', 'id', $newmodule->course)) {
         error('Course is misconfigured');
     }
-    if (! $cm = get_coursemodule_from_instance('ejercicio', $ejercicio->id, $course->id)) {
+    if (! $cm = get_coursemodule_from_instance('newmodule', $newmodule->id, $course->id)) {
         error('Course Module ID was incorrect');
     }
 
@@ -46,20 +46,20 @@ if ($id) {
 
 require_login($course, true, $cm);
 
-add_to_log($course->id, "ejercicio", "view", "view.php?id=$cm->id", "$ejercicio->id");
+add_to_log($course->id, "newmodule", "view", "view.php?id=$cm->id", "$newmodule->id");
 
 /// Print the page header
-$strejercicios = get_string('modulenameplural', 'ejercicio');
-$strejercicio  = get_string('modulename', 'ejercicio');
+$strnewmodules = get_string('modulenameplural', 'newmodule');
+$strnewmodule  = get_string('modulename', 'newmodule');
 
 $navlinks = array();
-$navlinks[] = array('name' => $strejercicios, 'link' => "index.php?id=$course->id", 'type' => 'activity');
-$navlinks[] = array('name' => format_string($ejercicio->name), 'link' => '', 'type' => 'activityinstance');
+$navlinks[] = array('name' => $strnewmodules, 'link' => "index.php?id=$course->id", 'type' => 'activity');
+$navlinks[] = array('name' => format_string($newmodule->name), 'link' => '', 'type' => 'activityinstance');
 
 $navigation = build_navigation($navlinks);
 
-print_header_simple(format_string($ejercicio->name), '', $navigation, '', '', true,
-              update_module_button($cm->id, $course->id, $strejercicio), navmenu($course, $cm));
+print_header_simple(format_string($newmodule->name), '', $navigation, '', '', true,
+              update_module_button($cm->id, $course->id, $strnewmodule), navmenu($course, $cm));
 
 /// Print the main part of the page
 
