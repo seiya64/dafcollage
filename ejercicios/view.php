4ce6272f7d62ad1cc0d090e6753d9a7b332fb96d
finamolina <finamolina@e382a061-ef60-41ce-baf5-d5b4294055cc>
2012-10-30 09:11:06 +0000

    Añadido asociación simple texto-texto
    git-svn-id: https://forja.rediris.es/svn/cusl6-dafcollag@595 e382a061-ef60-41ce-baf5-d5b4294055cc

--- ejercicios/view.php
+++ ejercicios/view.php
@@ -1,12 +1,40 @@
 <?php  // $Id: view.php,v 1.6.2.3 2009/04/17 22:06:25 skodak Exp $
 
-/**
- * This page prints a particular instance of ejercicios
- *
- * @author  Your Name <your@email.address>
- * @version $Id: view.php,v 1.6.2.3 2009/04/17 22:06:25 skodak Exp $
- * @package mod/ejercicios
- */
+/*
+  Daf-collage is made up of two Moodle modules which help in the process of
+  German language learning. It facilitates the content organization like
+  vocabulary or the main grammar features and gives the chance to create
+  exercises in order to consolidate knowledge.
+
+  Copyright (C) 2011
+
+  Coordination:
+  Ruth Burbat
+
+  Source code:
+  Francisco Javier Rodríguez López (seiyadesagitario@gmail.com)
+  Simeón Ruiz Romero (simeonruiz@gmail.com)
+  Serafina Molina Soto(finamolinasoto@gmail.com)
+
+  Original idea and content design:
+  Ruth Burbat
+  AInmaculada Almahano Güeto
+  Andrea Bies
+  Julia Möller Runge
+  Blanca Rodríguez Gómez
+  Antonio Salmerón Matilla
+  María José Varela Salinas
+  Karin Vilar Sánchez
+
+  This program is free software; you can redistribute it and/or
+  modify it under the terms of the GNU General Public License
+  as published by the Free Software Foundation; either version 2
+  of the License, or (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details. */
 
 /// (Replace ejercicios with the name of your module and remove this line)
 
@@ -24,7 +52,7 @@ $p  = optional_param('p', 0, PARAM_INT);
 $buscar= optional_param('buscar', 0, PARAM_INT);
 $id_ejercicio  = optional_param('id_ejercicio', PARAM_INT);
 $tipo_origen  = optional_param('tipo_origen', PARAM_INT);
-
+$trespuesta  = optional_param('tr', PARAM_INT);
 //Para busqueda
 
 $ccl = optional_param('ccl', -1, PARAM_INT);
@@ -81,7 +109,7 @@ print_header_simple(format_string($ejercicios->name), '', $navigation, '', '', t
 
 /// Print the main part of the page
 
-ejercicios_vista($cm->id,$op,$error,$name_ej,$tipo,$tipocreacion,$p,$id_ejercicio,$ccl,$cta ,$cdc,$cgr,$cic,$ctt,$buscar,$tipo_origen);
+ejercicios_vista($cm->id,$op,$error,$name_ej,$tipo,$tipocreacion,$p,$id_ejercicio,$ccl,$cta ,$cdc,$cgr,$cic,$ctt,$buscar,$tipo_origen,$trespuesta);
 
 
 /// Finish the page
