ea1ead088d785869d873727a371c5ea8570bbcba
finamolina <finamolina@e382a061-ef60-41ce-baf5-d5b4294055cc>
2012-10-30 09:08:18 +0000

    Añadido asociación simple texto-texto
    git-svn-id: https://forja.rediris.es/svn/cusl6-dafcollag@572 e382a061-ef60-41ce-baf5-d5b4294055cc

--- ejercicios/ejercicios_clases.php
+++ ejercicios/ejercicios_clases.php
@@ -14,11 +14,11 @@
   Source code:
   Francisco Javier Rodríguez López (seiyadesagitario@gmail.com)
   Simeón Ruiz Romero (simeonruiz@gmail.com)
-  Serafina Molina Soto (finamolinasoto@gmail.com)
- 
+  Serafina Molina Soto(finamolinasoto@gmail.com)
+
   Original idea and content design:
   Ruth Burbat
-  Inmaculada Almahano Güeto
+  AInmaculada Almahano Güeto
   Andrea Bies
   Julia Möller Runge
   Blanca Rodríguez Gómez
@@ -35,7 +35,6 @@
   but WITHOUT ANY WARRANTY; without even the implied warranty of
   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
   GNU General Public License for more details. */
-
 
 
 
