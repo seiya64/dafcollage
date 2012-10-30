11ae4b0b911e62186aeededddccd4e84e93cb6ea
finamolina <finamolina@e382a061-ef60-41ce-baf5-d5b4294055cc>
2012-10-30 09:09:15 +0000

    Añadido asociación simple texto-texto
    git-svn-id: https://forja.rediris.es/svn/cusl6-dafcollag@582 e382a061-ef60-41ce-baf5-d5b4294055cc

--- ejercicios/ejercicios_modificar_texto_texto.php
+++ ejercicios/ejercicios_modificar_texto_texto.php
@@ -15,7 +15,7 @@
   Francisco Javier Rodríguez López (seiyadesagitario@gmail.com)
   Simeón Ruiz Romero (simeonruiz@gmail.com)
   Serafina Molina Soto(finamolinasoto@gmail.com)
- 
+
   Original idea and content design:
   Ruth Burbat
   AInmaculada Almahano Güeto
