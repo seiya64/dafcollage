184aff458feca8fe2bd5fd975c452b7d6184e342
finamolina <finamolina@e382a061-ef60-41ce-baf5-d5b4294055cc>
2012-11-02 12:13:56 +0000

    Para modificar asociacion simple
    git-svn-id: https://forja.rediris.es/svn/cusl6-dafcollag@601 e382a061-ef60-41ce-baf5-d5b4294055cc

---
+++ ejercicios/ejercicios_modificar_asociacion_simple.php
@@ -0,0 +1,114 @@
+<?php
+/* 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/*
+  Daf-collage is made up of two Moodle modules which help in the process of
+  German language learning. It facilitates the content organization like
+  vocabulary or the main grammar features and gives the chance to create
+  exercises in order to consolidate knowledge.
+
+  Copyright (C) 2011
+
+  Coordination:
+  Ruth Burbat
+
+  Source code:
+  Francisco Javier Rodríguez López (seiyadesagitario@gmail.com)
+  Simeón Ruiz Romero (simeonruiz@gmail.com)
+  Serafina Molina Soto(finamolinasoto@gmail.com)
+
+  Original idea and content design:
+  Ruth Burbat
+  AInmaculada Almahano Güeto
+  Andrea Bies
+  Julia Möller Runge
+  Blanca Rodríguez Gómez
+  Antonio Salmerón Matilla
+  María José Varela Salinas
+  Karin Vilar Sánchez
+
+  This program is free software; you can redistribute it and/or
+  modify it under the terms of the GNU General Public License
+  as published by the Free Software Foundation; either version 2
+  of the License, or (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details. */
+
+require_once("../../config.php");
+require_once("lib.php");
+require_once("ejercicios_clase_general.php");
+require_once("ejercicios_form_creacion.php");
+
+$id_curso = optional_param('id_curso', 0, PARAM_INT);
+$id_ejercicio = optional_param('id_ejercicio', 0, PARAM_INT);
+$tipo_origen = optional_param('tipo_origen',0, PARAM_INT);
+$tipo_respuesta = optional_param('tr',0, PARAM_INT);
+$tipo_creacion = optional_param('tipocreacion',0, PARAM_INT);
+
+ECHO "MODIFICANDO";
+
+$mform = new mod_ejercicios_mostrar_ejercicio_asociacion_simple($id_curso,$id_ejercicio,$tipo_origen,$tipo_respuesta,$tipocreacion);
+$mform->mostrar_ejercicio_asociacion_simple($id_curso,$id_ejercicio,0,$tipo_origen,$tipo_respuesta,$tipocreacion);
+
+$numeropreguntas = optional_param('num_preg',0, PARAM_INT);
+
+echo "El numero de pregunas es".$numeropreguntas;
+
+
+    begin_sql();
+
+    if($tipo_respuesta==1){//Es un texto
+       
+            //obtengo los id de las preguntas del ejercicio
+            $id_preguntas=array();
+
+            $mis_preguntas= new Ejercicios_texto_texto_preg();
+
+            $id_preguntas=$mis_preguntas->obtener_todas_preguntas_ejercicicio($id_ejercicio);
+            //borro las respuestas
+
+            for($s=0;$s<sizeof($id_preguntas);$s++){
+                    delete_records('ejercicios_texto_texto_resp', 'id_pregunta', $id_preguntas[$s]->get('id'));
+
+            }
+
+    }
+
+    if($tipo_origen==1){
+            //borro las preguntas
+            delete_records('ejercicios_texto_texto_preg', 'id_ejercicio', $id_ejercicio);
+    }
+
+
+    //Guardo las nuevas
+
+    for($i=0;$i<$numeropreguntas;$i++){
+    //Obtengo el numero de respuestas a cada pregunta
+    $j=$i+1;
+
+
+        if($tipo_origen==1){ //Si la pregunta es un texto
+                $preg=required_param('pregunta'.$j,PARAM_TEXT);
+                $ejercicio_texto_preg = new Ejercicios_texto_texto_preg(NULL,$id_ejercicio,$preg);
+                $id_pregunta= $ejercicio_texto_preg->insertar();
+        }
+
+        if($tipo_respuesta==1){ //Si la respuesta es un texto
+            $resp=required_param('respuesta'.$j,PARAM_TEXT);
+
+            $correcta=0;
+
+           $ejercicio_texto_resp = new Ejercicios_texto_texto_resp(NULL,$id_pregunta,$resp,$correcta);
+           $ejercicio_texto_resp->insertar();
+        }
+    }
+    commit_sql();
+
+       redirect('./view.php?id=' . $id_curso . '&opcion=9');
+?>
