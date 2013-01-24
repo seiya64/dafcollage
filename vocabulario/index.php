510a8f18982dec2cc2b626997cc4fc6941ac1448
finamolina <finamolina@e382a061-ef60-41ce-baf5-d5b4294055cc>
2013-01-24 09:09:53 +0000

    idea original: Ruth
    git-svn-id: https://forja.rediris.es/svn/cusl6-dafcollag@684 e382a061-ef60-41ce-baf5-d5b4294055cc

--- vocabulario/index.php
+++ vocabulario/index.php
@@ -1,14 +1,42 @@
 <?php
+/*
+  Daf-collage is made up of two Moodle modules which help in the process of
+  German language learning. It facilitates the content organization like
+  vocabulary or the main grammar features and gives the chance to create
+  exercises in order to consolidate knowledge.
 
-// $Id: index.php,v 1.5 2006/08/28 16:41:20 mark-nielsen Exp $
-/**
- * This page lists all the instances of vocabulariovocabulario in a particular course
- *
- * @author 
- * @version $Id: index.php,v 1.5 2006/08/28 16:41:20 mark-nielsen Exp $
- * @package vocabulario
- * */
-/// Replace vocabulario with the name of your module
+  Copyright (C) 2011
+
+  Coordination:
+  Ruth Burbat
+
+  Source code:
+  Francisco Javier Rodríguez López (seiyadesagitario@gmail.com)
+  Simeón Ruiz Romero (simeonruiz@gmail.com)
+  Serafina Molina Soto(finamolinasoto@gmail.com)
+
+  Original idea:
+  Ruth Burbat
+
+  Content design:
+  Ruth Burbat
+  AInmaculada Almahano Güeto
+  Andrea Bies
+  Julia Möller Runge
+  Blanca Rodríguez Gómez
+  Antonio Salmerón Matilla
+  María José Varela Salinas
+  Karin Vilar Sánchez
+
+  This program is free software; you can redistribute it and/or
+  modify it under the terms of the GNU General Public License
+  as published by the Free Software Foundation; either version 2
+  of the License, or (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details. */
 
 require_once("../../config.php");
 require_once("lib.php");
