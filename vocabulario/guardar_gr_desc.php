f6a5a4939433e544976dfc3aac92ebe9d385ddd0
finamolina <finamolina@e382a061-ef60-41ce-baf5-d5b4294055cc>
2013-01-24 09:09:11 +0000

    idea original: Ruth
    git-svn-id: https://forja.rediris.es/svn/cusl6-dafcollag@679 e382a061-ef60-41ce-baf5-d5b4294055cc

--- vocabulario/guardar_gr_desc.php
+++ vocabulario/guardar_gr_desc.php
@@ -14,10 +14,14 @@
   Source code:
   Francisco Javier Rodríguez López (seiyadesagitario@gmail.com)
   Simeón Ruiz Romero (simeonruiz@gmail.com)
+  Serafina Molina Soto(finamolinasoto@gmail.com)
 
-  Original idea and content design:
+  Original idea:
   Ruth Burbat
-  Inmaculada Almahano Güeto
+
+  Content design:
+  Ruth Burbat
+  AInmaculada Almahano Güeto
   Andrea Bies
   Julia Möller Runge
   Blanca Rodríguez Gómez
