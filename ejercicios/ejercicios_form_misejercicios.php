b43997ff129e0b789d883ee76e8943cc53532556
finamolina <finamolina@e382a061-ef60-41ce-baf5-d5b4294055cc>
2013-01-25 10:50:50 +0000

    Asociacion Simple: Texto-Video funcionando
    git-svn-id: https://forja.rediris.es/svn/cusl6-dafcollag@702 e382a061-ef60-41ce-baf5-d5b4294055cc

--- ejercicios/ejercicios_form_misejercicios.php
+++ ejercicios/ejercicios_form_misejercicios.php
@@ -132,12 +132,18 @@ class mod_ejercicios_mis_ejercicios extends moodleform_mod {
 
                           }else{
 
-                              if($mi_ejercicio->get('tipoarchivorespuesta')==2){ //La respuesta es una imagen
+                              if($mi_ejercicio->get('tipoarchivorespuesta')==2){ //La respuesta es una audio
                                  $carpeta.='<li style="width:750px;"><a id="classa" href="./view.php?opcion=8&id='.$id.'&id_ejercicio='.$id_ejercicio.'&tipo_origen='.$mi_ejercicio->get('tipoarchivopregunta').'&tr='.$mi_ejercicio->get('tipoarchivorespuesta').'&tipocreacion='.$mi_ejercicio->get('tipoactividad').'">'. $nombre_ejercicio.'</a><a href="eliminar_carpetas_ejercicios.php?id_curso='.$id.'&id_ejercicio='.$id_ejercicio.'""><img src="./imagenes/delete.gif"/></a></li>';
 
                                 }else{
-                                   if($mi_ejercicio->get('tipoarchivorespuesta')==4){ //La respuesta es una imagen
+                                     if($mi_ejercicio->get('tipoarchivorespuesta')==3){ //La respuesta es un video
                                      $carpeta.='<li style="width:750px;"><a id="classa" href="./view.php?opcion=8&id='.$id.'&id_ejercicio='.$id_ejercicio.'&tipo_origen='.$mi_ejercicio->get('tipoarchivopregunta').'&tr='.$mi_ejercicio->get('tipoarchivorespuesta').'&tipocreacion='.$mi_ejercicio->get('tipoactividad').'">'. $nombre_ejercicio.'</a><a href="eliminar_carpetas_ejercicios.php?id_curso='.$id.'&id_ejercicio='.$id_ejercicio.'""><img src="./imagenes/delete.gif"/></a></li>';
+
+                                    }else{
+                                       if($mi_ejercicio->get('tipoarchivorespuesta')==4){ //La respuesta es una imagen
+                                         $carpeta.='<li style="width:750px;"><a id="classa" href="./view.php?opcion=8&id='.$id.'&id_ejercicio='.$id_ejercicio.'&tipo_origen='.$mi_ejercicio->get('tipoarchivopregunta').'&tr='.$mi_ejercicio->get('tipoarchivorespuesta').'&tipocreacion='.$mi_ejercicio->get('tipoactividad').'">'. $nombre_ejercicio.'</a><a href="eliminar_carpetas_ejercicios.php?id_curso='.$id.'&id_ejercicio='.$id_ejercicio.'""><img src="./imagenes/delete.gif"/></a></li>';
+
+                                        }
 
                                     }
                                 }
