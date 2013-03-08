0c7c19b2307012fe362c5b270cd5a7e68d7bf939
finamolina <finamolina@fimoso>
2013-03-08 18:02:37 +0100

    Completado fichero de idiomas español

--- ejercicios/lang/es_utf8/ejercicios.php
+++ ejercicios/lang/es_utf8/ejercicios.php
@@ -38,24 +38,115 @@
 $string['ejercicios'] = 'ejercicios';
 
 $string['modulename'] = 'ejercicios';
-$string['modulenameplural'] = 'ejercicios';
+$string['modulenameplural'] = 'ejerciciosS';
 
 $string['ejerciciosfieldset'] = 'Custom example fieldset';
 $string['ejerciciosintro'] = 'ejercicios Intro';
 $string['ejerciciosname'] = 'ejercicios Name';
 
 
-/**
- * Esto hay que traducirlo 
- */
-
-//Nombres ejercicios
-$string[Puzzledoble]='Puzzle doble';
+$string['Puzzledoble'] = 'Asociación';
+$string['CrearEjercicioPuzzle'] = 'Crear Ejercicio Asociación';
+$string['tituloeliminar'] = 'Crear Ejercicio Puzzle Doble';
 
 
-//botones
-$string[Aceptar]='Aceptar';
-$string[Cancelar]='Aceptar';
+$string['Opcionespuzzlealum'] = 'Ejercicios Asociación';
+$string['Nombre']='Nombre';
+$string['Imagen']='Numero de imagenes';
+$string['Aceptar']='Aceptar';
+$string['Reset']='Menu Principal';
+$string['Borrar']='Borrar';
+$string['TextoEliminar']='Seleccione el ejercicio a eliminar';
 
 
+//Interfaz Alumno
+
+$string['Buscar']='Buscar actividades:';
+$string['Boton_Buscar']='Buscar';
+$string['Actividades']='Actividades';
+$string['Tema']='Campo Temático';
+$string['Tipo de Actividad']='Tipo de Actividad';
+$string['Destreza comunicativa']='Destreza comunicativa';
+$string['Gramatica']='Tema Gramatical';
+$string['Realizar']='Realizar';
+$string['Crear']='Crear Actividades:';
+$string['creando']='Creando ejercicio por Tema:';
+$string['Boton_Crear']='Crear';
+$string['MisActividades']='Mis Actividades';
+$string['ActividadesCurso']='Actividades de mi Curso';
+$string['EjerciciosCurso']='Ejercicios propuestos';
+
+$string['Volver']='Atrás';
+$string['Intencion']='Intención Comunicativa';
+
+
+//CLASIFICACION TIPO DE ACTIVIDAD
+$string['Tipo0']="Seleccionar";
+$string['Tipo1']="--";
+$string['Tipo2']="ELECCIÓN MÚLTIPLE con sus diversas variantes";
+$string['Tipo3']="ASOCIACIÓN SIMPLE";
+$string['Tipo4']="ASOCIACIÓN COMPLEJA";
+$string['Tipo5']="TEXTO HUECO";
+$string['Tipo6']="IDENTIFICAR ELEMENTOS";
+$string['Tipo7']="RESPUESTA ABIERTA";
+$string['Tipo8']="CRUCIGRAMA";
+$string['Tipo9']="ORDENAR ELEMENTOS";
+$string['Tipo10']="IDENTIFICAR ELEMENTOS MÁS RESPUESTA CORTA";
+$string['Tipo11']="IDENTIFICAR ELEMENOS CON ASOCIACIÓN SIMPLE";
+$string['Tipo12']="IDENTIFICAR ELEMENOS CON  RESPUESTA MÚLTIPLE";
+$string['Tipo13']="PRACTICAR PRONUNCIACIÓN";
+
+
+
+//CLASIFICACION DESTREZA
+$string['Destreza0']="Seleccionar";
+$string['Destreza1']="--";
+$string['Destreza2']="COMPRENSIÓN LECTORA";
+$string['Destreza3']="COMPRENSIÓN ORAL";
+$string['Destreza4']="EXPRESIÓN ESCRITA";
+$string['Destreza5']="EXPRESIÓN ORAL";
+$string['Destreza6']="TRADUCCIÓN";
+
+
+$string['componerActividades']="Crear Conjunto de Actividades";
+
+//CREANDO EJERICIO FORMULARIO GENERICO
+
+$string["FormularioCreacion"]= "Formulario de Creación";
+$string["tipopregunta"]='Tipo de archivo pregunta:';
+$string["numeropreguntas"]="Número total de archivos pregunta:";
+$string["tiporespuesta"]='Tipo de archivo respuesta:';
+$string["numerorespuestas"]="Número de archivos respuesta por pregunta:";
+$string["numerorespuestascorrectas"]="Número de archivos respuesta correctos:";
+$string["nombre"]="Titule su ejercicio para facilitar la identificación o búsqueda:";
+$string["descripcion"]="Añade una breve introducción al ejercicio:";
+$string["visible"]="Visible (accesible por sus alumnos)";
+$string["publico"]="Público (accesible por otros profesores y alumnos)";
+$string["carpeta"]="Introduzca la ruta del ejercicio";
+$string["copyright"]="Seleccione el copyright de sus archivos respuesta";
+$string["copyrightresp"]="Seleccione el copyright de sus archivos pregunta";
+$string["textoclasej"]="Asigne una o varias categorías de búsqueda de los siguientes menús desplegables(Obligatorio Campo Temático o Tema gramatical):";
+//Formulario de creacion texto-texto
+
+$string["FormularioCreacionTextos"]="Introduzca las preguntas y respuestas del ejercicio";
+$string["pregunta"]="Pregunta ";
+$string["respuesta"]="Respuesta ";
+$string["Correcta"]="Correcta ";
+$string["BotonAñadirRespuesta"]="Añadir otra Respuesta";
+
+$string["BotonCorregir"]="Corregir";
+$string["BotonGuardar"]="Guardar";
+
+
+
+//Mis ejercicios(Ejercicios profesor)
+$string["MisEjercicios"]="Mis Ejercicios";
+$string["Ejercicios"]="Ejercicios";
+$string["MiBusqueda"]="Mi Busqueda";
+
+
+//Buscando
+$string["BotonAñadir"]="Añadir a mis ejercicios";
+$string['Misejercicios']='Mis Actividades';
+$string["marcoteorico"]="Seleccionar el nivel según el marco europeo de referecia(OPCIONAL)";
 ?>
