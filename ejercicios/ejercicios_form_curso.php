de7c1c99ab6339d986c47c49c744523d68585f93
finamolina <finamolina@fimoso>
2013-03-20 13:51:25 +0100

    Arregladas cosas

--- ejercicios/ejercicios_form_curso.php
+++ ejercicios/ejercicios_form_curso.php
@@ -127,7 +127,7 @@ class mod_ejercicios_curso extends moodleform_mod{
                                    if($todos_ejer_curso[$i]->get('tipoarchivorespuesta')==3){ //La respuesta es una imagen
                                      $carpeta.='<li style="width:750px;"><a id="classa" href="./view.php?opcion=8&id='.$id.'&id_ejercicio='.$id_ejercicio.'&buscar=1&tipo_origen='.$todos_ejer_curso[$i]->get('tipoarchivopregunta').'&tr='.$todos_ejer_curso[$i]->get('tipoarchivorespuesta').'&tipocreacion='.$todos_ejer_curso[$i]->get('tipoactividad').'">'. $nombre_ejercicio.'</a></li>';
                                    }else{
-                                      if($todos_ejer_curso[$i]->get('tipoarchivorespuesta')==4){ //La respuesta es una imagen
+                                      if($todos_ejer_curso[$i]->get('tipoarchivorespuesta')==4){ //La respuesta es una foto
                                          $carpeta.='<li style="width:750px;"><a id="classa" href="./view.php?opcion=8&id='.$id.'&id_ejercicio='.$id_ejercicio.'&buscar=1&tipo_origen='.$todos_ejer_curso[$i]->get('tipoarchivopregunta').'&tr='.$todos_ejer_curso[$i]->get('tipoarchivorespuesta').'&tipocreacion='.$todos_ejer_curso[$i]->get('tipoactividad').'">'. $nombre_ejercicio.'</a></li>';
 
                                       }
@@ -148,7 +148,12 @@ class mod_ejercicios_curso extends moodleform_mod{
                                      $carpeta.='<li style="width:750px;"><a id="classa" href="./view.php?opcion=8&id='.$id.'&id_ejercicio='.$id_ejercicio.'&buscar=1&tipo_origen='.$todos_ejer_curso[$i]->get('tipoarchivopregunta').'&tr='.$todos_ejer_curso[$i]->get('tipoarchivorespuesta').'&tipocreacion='.$todos_ejer_curso[$i]->get('tipoactividad').'">'. $nombre_ejercicio.'</a></li>';
 
                               }
-                         }
+                             }else{
+                                  if($todos_ejer_curso[$i]->get('tipoarchivorespuesta')==1){ //La respuesta es un texto
+                                     $carpeta.='<li style="width:750px;"><a id="classa" href="./view.php?opcion=8&id='.$id.'&id_ejercicio='.$id_ejercicio.'&buscar=1&tipo_origen='.$todos_ejer_curso[$i]->get('tipoarchivopregunta').'&tr='.$todos_ejer_curso[$i]->get('tipoarchivorespuesta').'&tipocreacion='.$todos_ejer_curso[$i]->get('tipoactividad').'">'. $nombre_ejercicio.'</a></li>';
+
+                                }
+                             }
                         }
 
 
