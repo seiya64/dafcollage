2f34057325d1d8889f8e18707d6fa6b7af35ab4b
finamolina <finamolina@fimoso>
2013-03-20 13:56:46 +0100

    Arregladas cosas

--- ejercicios/ejercicios_form_curso.php
+++ ejercicios/ejercicios_form_curso.php
@@ -149,9 +149,10 @@ class mod_ejercicios_curso extends moodleform_mod{
 
                               }
                              }else{
+                                 if($todos_ejer_curso[$i]->get('tipoarchivopregunta')==4){ //La pregunta es un IMAGEN
                                   if($todos_ejer_curso[$i]->get('tipoarchivorespuesta')==1){ //La respuesta es un texto
                                      $carpeta.='<li style="width:750px;"><a id="classa" href="./view.php?opcion=8&id='.$id.'&id_ejercicio='.$id_ejercicio.'&buscar=1&tipo_origen='.$todos_ejer_curso[$i]->get('tipoarchivopregunta').'&tr='.$todos_ejer_curso[$i]->get('tipoarchivorespuesta').'&tipocreacion='.$todos_ejer_curso[$i]->get('tipoactividad').'">'. $nombre_ejercicio.'</a></li>';
-
+                                  }
                                 }
                              }
                         }
