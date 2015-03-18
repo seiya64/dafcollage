d9d359fcabc3fa6bd3aedf04825970546114724e
ramon-rd <ramonruedadelgado@gmail.com>
2015-03-18 09:31:30 +0100

    Creación del fichero separar_tablas. Se utilizará para la migración de la base de datos de moodle 1.9 a moodle 2.8

---
+++ vocabulario/db/separar_tablas.php
@@ -0,0 +1,60 @@
+<?php
+
+/*
+  Daf-collage is made up of two Moodle modules which help in the process of
+  German language learning. It facilitates the content organization like
+  vocabulary or the main grammar features and gives the chance to create
+  exercises in order to consolidate knowledge.
+
+  Copyright (C) 2011
+
+  Coordination:
+  Ruth Burbat
+
+  Source code:
+  Ramon Rueda Delgado (ramonruedadelgado@gmail.com)
+  Luis Redondo Exposito (luis.redondo.exposito@gmail.com)
+
+  Original idea:
+  Ruth Burbat
+
+
+  This program is free software; you can redistribute it and/or
+  modify it under the terms of the GNU General Public License
+  as published by the Free Software Foundation; either version 2
+  of the License, or (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details. */
+
+//Función que se encarga de separar las tablas de una copia de seguridad de 
+//moodle 1.9, con el objetivo de poder migrar la base de datos de moodle 1.9 a 
+//moodle 2.8.
+
+//Función que separa las tablas de vocabulario moodle 1.9 del resto de las tablas.
+function obtenerTablasVocabulario($nombreFichero)
+{
+    
+}
+
+//Función que separa las tablas de moodle 1.9 del resto de las tablas.
+function obtenerTablasMoodle19($nombreFichero)
+{
+    
+}
+
+//Función que transforma las tablas pertenecientes a moodle 1.9 a tablas con la
+//estructura de moodle 2.8
+function transformarTablas19To28($nombreFichero)
+{
+    
+}
+
+//Función que devuelve un fichero más pequeño. Inicialmente el fichero de la 
+//copia de seguridad era muy grande y muy costoso de abrir.
+function partirFichero($nombreFichero)
+{
+    
+}
